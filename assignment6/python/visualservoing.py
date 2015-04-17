3a330663bd4cf8aa58d20cce78204aa8547a4944
Alex Gurney <thaeds@gmail.com>
2015-04-16 21:56:41 -0400

    current status

--- assignment6/python/visualservoing.py
+++ assignment6/python/visualservoing.py
@@ -100,21 +100,31 @@ def getRT(corners):
     # defines the corners of the object we are tracking in x, y, z space in inches
     objectpoints = np.array([[1, -1, 0], [-1, -1, 0], [-1, 1, 0], [1, 1, 0]], np.float32)
     # objectpoints = np.array([[12, 8, 2], [12, 8, -2], [12, 4, -2], [12, 4, 2]], np.float32)
-    kmat = np.array([[1211.2959, 0, 657.15924], [0, 1206.00512, 403.17667], [0, 0, 1]])
-    F, R, T = cv2.solvePnP(objectpoints, corners, kmat, None)
+    # kmat = np.array([[1211.2959, 0, 657.15924], [0, 1206.00512, 403.17667], [0, 0, 1]])
+    # F, R, T = cv2.solvePnP(objectpoints, corners, kmat, None)
     # print "Rvec"
     # print R
     # print "Tvec"
     # print T
-    print "R"
-    R = R[1]
-    print R #Y rotation
 
-    # print "T"
-    # print T
-    print "T"
-    T = np.append(T[0], T[2])
-    print T
+    F = True
+    if None is corners:
+        F = None
+    centerx = sum(corners[i][0] for i in range(4))/4
+    centery = sum(corners[i][1] for i in range(4))/4
+    center = [centerx, centery]
+
+    heightleft = corners[2][1] - corners[1][1]
+    heightright = corners[3][1] - corners[0][1]
+
+
+
+    # Xpix = 1280 Ypix = 800
+
+
+    # print corners
+    # print centerx
+    # print centery
 
     return F, R, T
 
@@ -125,6 +135,22 @@ def getRT(corners):
 # # F, R, T = getRT(sq1)
 # cv2.imshow('Start', img1)
 
+# demo!
+sq1, img1 = find_black_square('../start.jpg')
+sq2, img2 = find_black_square('../goal.jpg')
+print "5"
+# sq5, img5 = find_black_square('test-images/pic-5.jpg')
+# F, R, T = getRT(sq5)
+# cv2.imshow('5', img5)
+sq6, img6 = find_black_square('test-images/pic-6.jpg')
+F, R, T = getRT(sq6)
+cv2.imshow('6', img6)
+# sq8, img8 = find_black_square('test-images/pic-8.jpg')
+# cv2.imshow('8', img8)
+# F, R, T = getRT(sq8)
+cv2.waitKey()
+
+#
 # cv2.waitKey()
 # cv2.imshow('Goal', img2)
 # F, R, T = getRT(sq2)
@@ -156,7 +182,9 @@ def drive_forward(scrib, inches):
 def line_up(theta, trans, targetPos):
     pass
     # turn in the direction of the new position we should be in
-    # TODO:
+    disttoorigin = np.sqrt(trans[0]**2 + trans[1]**2)
+    firstrotation = r
+
 
     # move the right amount to get there
     # TODO:
